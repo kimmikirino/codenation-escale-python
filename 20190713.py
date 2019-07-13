960f822c5adcc545ae74d8cf76eb8eb4af09d057
Jaqueline Kirino <bfatskih@gmail.com>
2019-07-13 15:14:16 -0300

    first class

---
+++ 20190713.py
@@ -0,0 +1,66 @@
+# name = input('Qual o seu nome?')
+# print('olá %s' %name)
+# print(f'olá {name}')
+# print('olá {}'.format(name))
+
+# palavras reservadas
+# import keyword
+# print(keyword.kwlist)
+
+# snake nome_completo
+
+# tipo de dados
+# string, int, float, complex, bool
+
+# email = 'aaaa@aaa.com'
+# print(type(email)) # str
+
+# age = 10
+# print(type(age)) # int
+
+# price = 3.0
+# print(type(price)) # float
+
+# complex_value = 3 + 6j
+# print(type(complex_value)) # numeros irracionais, complex
+
+# has_food = True
+# print(type(has_food)) # bool
+
+# print('abacate'.upper()) # maiuscula
+# print('Jao da SilvaAAAAA'.lower()) # minuscula
+# print('kimmi kirino'.capitalize()) # primeira letra maiuscula
+# print('kimmi kirino'.title()) # primeira letra de cada palavra da string
+
+# string como array
+# bolo = 'bolo de cenoura'
+# print(bolo[0]) # posição 0 = b
+# print(bolo[-2]) # posição de tras para frente = r
+# print(bolo[:3]) # os tres primeiros = bol
+# print(bolo[1:4]) # os primeiros = bolo 
+# print(bolo[0:6:2]) # pega a posição zero e pula de dois em dois até a 6 = bl
+# print(bolo[::-1]) # inverte a string
+
+# replace 
+# print('string'.replace('i', 'o')) # strong
+# print('string'.find('i')) # retorna index
+# print('string'.find('i', 1)) # retorna index, segundo parametro é onde inicia
+# print('banana'.find('i')) # retorna index -1 quando não encontra
+
+# Data Types
+# Exerício, defina as variaveis e verifique os tipos
+# nome = 'Jaqueline'
+# idade = 32
+# nota = 8.75
+# aprovada = True
+
+# print(type(nome)) # str
+# print(type(idade)) # int
+# print(type(nota)) # float
+# print(type(aprovada)) # bool
+
+# exercicio 2
+serie = 'stranger things'
+print(serie.upper()) # defina em maiúsculo
+print(serie.title()) # primeira letra de cada maiusculo
+print(serie[::-1]) # palavra invertida
