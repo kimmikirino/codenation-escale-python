ae69103ebe1a23d9dfa4016d3e75e574297aa01c
Jaqueline Kirino <bfatskih@gmail.com>
2019-07-20 11:01:18 -0300

    for

---
+++ 20190720.py
@@ -0,0 +1,7 @@
+# dada a lista de linguagens imprima na tela a linguagem que começa com a letra p   
+linguagens = ['Java', 'PHP', 'C', 'Python']
+
+for item in linguagens:
+    if item.upper().startswith('P') :
+        print(item)
+
